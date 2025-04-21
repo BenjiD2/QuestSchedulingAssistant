fd197cc81d4946a81d4e92fb65b9c28a54f34d52
Kanchan Naik <kanchan@uchicago.edu>
2025-04-21 18:11:19 -0500

    initial architecture

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/api/hello')
+      .then(res => res.json())
+      .then(data => setMessage(data.message))
+      .catch(err => console.error('Error fetching from API:', err));
+  }, []);
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>React + Express</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
+
